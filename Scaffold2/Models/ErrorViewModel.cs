54d5d6f31b51666a74de562cb2226dab299d41c2
arthurshk <arthurdragon39@gmail.com>
2023-08-28 15:19:17 +0300

    Add project files.

---
+++ Scaffold2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Scaffold2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
