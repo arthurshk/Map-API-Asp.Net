be87776126c102f8936e663985e2df74f05db723
arthurshk <arthurdragon39@gmail.com>
2023-09-11 00:49:59 +0300

    commit

--- Scaffold2/Controllers/CalendarController.cs
+++ Scaffold2/Controllers/CalendarController.cs
@@ -15,13 +15,13 @@ namespace Scaffold2.Controllers
         {
             return View(db.Calendar.ToList());
         }
-        [HttpGet("calendar/create")]
+        [HttpGet("/calendar/create")]
         public IActionResult Create(CalendarModel calendarModel)
         {
             return View(calendarModel);
         }
 
-        [HttpPost("calendar/create")]
+        [HttpPost("/calendar/create")]
         public IActionResult CreateEvent(CalendarModel calendarModel)
         {
             if (ModelState.IsValid)
@@ -32,16 +32,33 @@ namespace Scaffold2.Controllers
             }
             return View("Create", calendarModel);
         }
-        //[HttpDelete("calendar/delete")]
-        //public IActionResult DeleteEvent(CalendarModel calendarModel)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Calendar.Add(calendarModel);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View("Create", calendarModel);
-        //}
+        [HttpGet("/calendar/edit/{Id}")]
+        public IActionResult Edit(int Id)
+        {
+            var calEvent = db.Calendar.FirstOrDefault(x => x.Id == Id);
+            return View(calEvent);
+        }
+        [HttpPost("/calendar/edit/{Id}")]
+        public IActionResult Edit(int Id, [FromBody]CalendarModel calendarModel)
+        {
+            var calEvent = db.Calendar.FirstOrDefault(x => x.Id == Id);
+            calEvent.eventTitle = calendarModel.eventTitle;
+            calEvent.eventDate = calendarModel.eventDate;
+            calEvent.eventDescription = calendarModel.eventDescription;
+            if (!ModelState.IsValid)
+            {
+                return View("Index", calendarModel);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpDelete("/calendar/delete/{Id}")]
+        public IActionResult DeleteEvent(int id)
+        {
+            var calEvent = db.Calendar.First(x => x.Id == id);
+            db.Calendar.Remove(calEvent);
+            db.SaveChanges();
+            return Ok(new { Ok = true });
+        }
     }
 }
